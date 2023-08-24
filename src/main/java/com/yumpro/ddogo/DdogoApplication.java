c1a6528c5cdbd9377462bc6cfeef920e2428c2ac
informationRam <qhfka0527@gmail.com>
2023-08-24 21:13:50 +0900

    first commit

---
+++ src/main/java/com/yumpro/ddogo/DdogoApplication.java
@@ -0,0 +1,13 @@
+package com.yumpro.ddogo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdogoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdogoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yumpro/ddogo/DdogoApplicationTests.java
@@ -0,0 +1,23 @@
+package com.yumpro.ddogo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class DdogoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void test() {
+
+	}
+
+}
