4d237e8bd50e5dad4bc041fe35801ac0005515f3
Yujin Ahn <ttangppy@gmail.com>
2023-08-28 20:01:11 +0900

    weekend work8/28

--- src/main/java/com/yumpro/ddogo/myMapController/geoloController.java
+++ src/main/java/com/yumpro/ddogo/myMapController/geoloController.java
@@ -1,4 +1,4 @@
-package com.yumpro.ddogo.controller;
+package com.yumpro.ddogo.myMapController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
