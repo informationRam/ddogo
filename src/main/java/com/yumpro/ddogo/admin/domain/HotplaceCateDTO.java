9ac374ac19cd56e17bc3e006fa4f35ba0762893c
Cha22un <2chaeun522@gmail.com>
2023-08-29 09:47:35 +0900

    0829

---
+++ src/main/java/com/yumpro/ddogo/admin/domain/HotplaceCateDTO.java
@@ -0,0 +1,12 @@
+package com.yumpro.ddogo.admin.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class HotplaceCateDTO {
+    private int hotplace_cate_no;
+
+    private String hotplace_cate_name;
+}
