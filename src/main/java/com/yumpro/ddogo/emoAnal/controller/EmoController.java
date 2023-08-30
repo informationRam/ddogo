aaf0639dd4f0e6a2c5c5bd20f281a5f8dd9d36cc
min <happysss0505@gmail.com>
2023-08-30 20:53:26 +0900

    8/30 감정분석 컨트롤러,서비스 수정

--- src/main/java/com/yumpro/ddogo/emoAnal/controller/EmoController.java
+++ src/main/java/com/yumpro/ddogo/emoAnal/controller/EmoController.java
@@ -25,99 +25,26 @@ public class EmoController {
 
     private final EmoService emoService;
 
+    //리뷰 폼 보여주기
     @GetMapping("/reviewadd")
     public String add(ReviewForm ReviewForm) {
         return "emoAnal/emoReviewForm";
     }
 
+
+    //리뷰 등록 처리(감정분석과 함께)
     @PostMapping("/reviewadd")
     public String emoreview(@Valid ReviewForm reviewForm, BindingResult bindingResult, Emoreview emoreview) {
-
-        double emo_result = 0;
-        try {
-            // API 엔드포인트 URL
-            String url = "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze";
-
-            // API 키 정보
-            String apiKeyId = "edivdod8s8";
-            String apiKey = "yDRlMYWzUFAW0LpcrsABRLikhHqMNtHof3pCasuI";
-
-            // 요청 데이터 생성
-            JSONObject data = new JSONObject();
-            data.put("content", reviewForm.getReview());
-
-            // POST 요청 설정
-            URL apiUrl = new URL(url);
-            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setRequestProperty("X-NCP-APIGW-API-KEY-ID", apiKeyId);
-            connection.setRequestProperty("X-NCP-APIGW-API-KEY", apiKey);
-            connection.setDoOutput(true);
-
-            // 요청 데이터 전송
-            try (OutputStream os = connection.getOutputStream()) {
-                byte[] input = data.toString().getBytes("utf-8");
-                os.write(input, 0, input.length);
-            }
-
-            // 응답 읽기
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                StringBuilder response = new StringBuilder();
-                try (java.io.BufferedReader in = new java.io.BufferedReader(
-                        new java.io.InputStreamReader(connection.getInputStream()))) {
-                    String line;
-                    while ((line = in.readLine()) != null) {
-                        response.append(line);
-                    }
-                }
-                //JSONObject result = new JSONObject(response.toString());
-                String jsonString = response.toString(); // Replace with your JSON response string
-                JSONObject result2 = new JSONObject(jsonString);
-
-                // Extracting document object
-                JSONObject document = result2.getJSONObject("document");
-
-                // Extracting sentiment and confidence from the document object
-                String sentiment = document.getString("sentiment");
-                JSONObject confidence = document.getJSONObject("confidence");
-                float negativeConfidence = (float) confidence.getDouble("negative");
-                float neutralConfidence = (float) confidence.getDouble("neutral");
-                float positiveConfidence = (float) confidence.getDouble("positive");
-                emo_result = Math.round(positiveConfidence * 100) / 100.0; //소수점 둘째자리까지 보여줌.
-
-                // Printing extracted data
-                System.out.println("Document Sentiment: " + sentiment);
-                System.out.println("Negative Confidence: " + negativeConfidence);
-                System.out.println("Neutral Confidence: " + neutralConfidence);
-                System.out.println("Positive Confidence: " + positiveConfidence);
-                System.out.println("결과=" + emo_result);
-
-            } else {
-                System.out.println("요청 실패 - 상태 코드: " + responseCode);
-                try (java.io.BufferedReader errorIn = new java.io.BufferedReader(
-                        new java.io.InputStreamReader(connection.getErrorStream()))) {
-                    String line;
-                    StringBuilder errorMsg = new StringBuilder();
-                    while ((line = errorIn.readLine()) != null) {
-                        errorMsg.append(line);
-                    }
-                    System.out.println("에러 메시지: " + errorMsg.toString());
-                }
-            }
-
-            // 연결 종료
-            connection.disconnect();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        //1.파라미터받기
+        //2.비지니스로직수행
+        //2-1.감정분석 수행
+        double emo_result = emoService.emoAnal(emoreview);
         if (bindingResult.hasErrors()) { //에러가 존재하면
             return "emoAnal/emoReviewForm";  //emoReviewForm.html문서로 이동
         }
-
+        //2-2.리뷰등록
         emoService.updateReview(reviewForm.getReview(), reviewForm.getHotplace_no(), reviewForm.getMap_no(), emo_result);
+
         return "emoAnal/imsi"; //리뷰등록 성공하면
     }
 
--- src/main/java/com/yumpro/ddogo/emoAnal/service/EmoService.java
+++ src/main/java/com/yumpro/ddogo/emoAnal/service/EmoService.java
@@ -3,7 +3,12 @@ package com.yumpro.ddogo.emoAnal.service;
 import com.yumpro.ddogo.emoAnal.entity.Emoreview;
 import com.yumpro.ddogo.emoAnal.repository.EmoRepository;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Service
 @RequiredArgsConstructor
@@ -11,6 +16,7 @@ public class EmoService {
 
     private final EmoRepository emoRepository;
 
+    //리뷰등록(임시)
     public void updateReview(String reveiw,int hotplace_no,int map_no,double emo_result){
         Emoreview emoreview = new Emoreview();
         emoreview.setReview(reveiw);
@@ -20,11 +26,89 @@ public class EmoService {
         emoRepository.save(emoreview);
     }
 
-    public void add(String reveiw,int hotplace_no,int map_no){
-        Emoreview emoreview = new Emoreview();
-        emoreview.setReview(reveiw);
-        emoreview.setHotplace_no(hotplace_no);
-        emoreview.setMap_no(map_no);
-        emoRepository.save(emoreview);
+    //감정분석
+    public double emoAnal(Emoreview emoreview){
+        double emo_result = 0;
+        try {
+            // API 엔드포인트 URL
+            String url = "https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze";
+
+            // API 키 정보
+            String apiKeyId = "edivdod8s8";
+            String apiKey = "yDRlMYWzUFAW0LpcrsABRLikhHqMNtHof3pCasuI";
+
+            // 요청 데이터 생성
+            JSONObject data = new JSONObject();
+            data.put("content", emoreview);
+
+            // POST 요청 설정
+            URL apiUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setRequestProperty("X-NCP-APIGW-API-KEY-ID", apiKeyId);
+            connection.setRequestProperty("X-NCP-APIGW-API-KEY", apiKey);
+            connection.setDoOutput(true);
+
+            // 요청 데이터 전송
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = data.toString().getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+
+            // 응답 읽기
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                StringBuilder response = new StringBuilder();
+                try (java.io.BufferedReader in = new java.io.BufferedReader(
+                        new java.io.InputStreamReader(connection.getInputStream()))) {
+                    String line;
+                    while ((line = in.readLine()) != null) {
+                        response.append(line);
+                    }
+                }
+                //JSONObject result = new JSONObject(response.toString());
+                String jsonString = response.toString(); // Replace with your JSON response string
+                JSONObject result2 = new JSONObject(jsonString);
+
+                // Extracting document object
+                JSONObject document = result2.getJSONObject("document");
+
+                // Extracting sentiment and confidence from the document object
+                String sentiment = document.getString("sentiment");
+                JSONObject confidence = document.getJSONObject("confidence");
+                float negativeConfidence = (float) confidence.getDouble("negative");
+                float neutralConfidence = (float) confidence.getDouble("neutral");
+                float positiveConfidence = (float) confidence.getDouble("positive");
+                emo_result = Math.round(positiveConfidence * 100) / 100.0; //소수점 둘째자리까지 보여줌.
+
+                // Printing extracted data
+                System.out.println("Document Sentiment: " + sentiment);
+                System.out.println("Negative Confidence: " + negativeConfidence);
+                System.out.println("Neutral Confidence: " + neutralConfidence);
+                System.out.println("Positive Confidence: " + positiveConfidence);
+                System.out.println("결과=" + emo_result);
+
+            } else {
+                System.out.println("요청 실패 - 상태 코드: " + responseCode);
+                try (java.io.BufferedReader errorIn = new java.io.BufferedReader(
+                        new java.io.InputStreamReader(connection.getErrorStream()))) {
+                    String line;
+                    StringBuilder errorMsg = new StringBuilder();
+                    while ((line = errorIn.readLine()) != null) {
+                        errorMsg.append(line);
+                    }
+                    System.out.println("에러 메시지: " + errorMsg.toString());
+                }
+            }
+
+            // 연결 종료
+            connection.disconnect();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return emo_result;
     }
+
+
 }
