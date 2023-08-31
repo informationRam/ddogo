74b1e57a19c4ab48465ae66f936f41b2ef6665f6
Cha22un <2chaeun522@gmail.com>
2023-08-31 17:39:05 +0900

    0830

--- src/main/java/com/yumpro/ddogo/admin/domain/RankingDTO.java
+++ src/main/java/com/yumpro/ddogo/admin/domain/RankingDTO.java
@@ -6,13 +6,12 @@ import lombok.*;
 @Setter
 @AllArgsConstructor
 @ToString
-public class HotplaceDTO {
+public class RankingDTO {
     private int hotplace_no;
     private String sido;
     private String gugun;
     private String hotplace_name;
-    private String address;
-    private int hotplace_cate_no;
-    private double lat;
-    private double lng;
+    private String hotplace_cate_name;
+    private int recom_count;
+    private double emo_result;
 }
--- src/main/resources/static/js/admin/script.js
+++ src/main/resources/static/js/admin/script.js
@@ -1,15 +1,18 @@
-const charts = document.querySelectorAll(".chart");
+var dynamicLabels = ["Label 1", "Label 2", "Label 3", "Label 4", "Label 5"];
+var dynamicData = [10, 20, 5, 15, 8];
+
+const charts = document.querySelectorAll(".year");
 
 charts.forEach(function (chart) {
   var ctx = chart.getContext("2d");
   var myChart = new Chart(ctx, {
     type: "bar",
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      labels: dynamicLabels, // 동적으로 변경된 라벨 데이터
       datasets: [
         {
           label: "# of Votes",
-          data: [12, 19, 3, 5, 2, 3],
+          data: dynamicData, // 동적으로 변경된 데이터 데이터
           backgroundColor: [
             "rgba(255, 99, 132, 0.2)",
             "rgba(54, 162, 235, 0.2)",
