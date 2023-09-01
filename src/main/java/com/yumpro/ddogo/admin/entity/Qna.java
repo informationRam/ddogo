c04172c573226f22ef314196cc43287f9e8210a6
Cha22un <2chaeun522@gmail.com>
2023-09-01 18:29:11 +0900

    0901

--- src/main/java/com/yumpro/ddogo/admin/entity/Qna.java
+++ src/main/java/com/yumpro/ddogo/admin/entity/Qna.java
@@ -1,14 +1,36 @@
 package com.yumpro.ddogo.admin.entity;
 
-public class Qna {
-    CREATE TABLE `qna` (
-            `qna_no` int NOT NULL AUTO_INCREMENT COMMENT '문의 번호. 문의 글 번호',
-            `qna_title` varchar(100) NOT NULL COMMENT '문의 제목',
-            `qna_content` text NOT NULL COMMENT '문의 내용',
-            `user_no` int NOT NULL COMMENT '회원 번호',
-            `qa_solved` char(1) NOT NULL DEFAULT 'N' COMMENT '문의 확인여부',
-            `qna_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '문의 일',
-            `qna_pwd` varchar(50) NOT NULL COMMENT '문의 비밀번호',
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@ToString
+public class Qna {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private int qna_no;
+
+    @Column
+    private String qna_title;
+
+    @Column
+    private String qna_content;
+
+    @ManyToOne
+    private int user_no;
+
+    @OneToMany
+    private String qa_solved;
+
+    @Column
+    private Date qna_date;
+
+    @Column
+    private String qna_pwd;
 }
