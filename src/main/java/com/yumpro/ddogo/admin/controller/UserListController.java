c24c96fc202404bdeb8f7aeabd8a4e326e7776ce
Cha22un <2chaeun522@gmail.com>
2023-09-04 16:17:56 +0900

    0904

--- src/main/java/com/yumpro/ddogo/admin/controller/UserListController.java
+++ src/main/java/com/yumpro/ddogo/admin/controller/UserListController.java
@@ -26,6 +26,7 @@ public class UserListController {
         map.put("limit", limit);
         map.put("offset", offset);
 
+        System.out.println("검색어는 ?!?! "+map.get("search"));
         List<UserDTO> userList = userService.userList(map);
 
         int totalPages = (int) Math.ceil((double) userList.size() / limit);
