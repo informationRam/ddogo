a2b68b573be612c55ff2b8f4781e02780044aa31
ryuahneee <ryuahneee@gmail.com>
2023-09-04 19:53:07 +0900

    commit 230904- 공지사항 수정 처리 완료

--- src/main/java/com/yumpro/ddogo/notice/service/NoticeService.java
+++ src/main/java/com/yumpro/ddogo/notice/service/NoticeService.java
@@ -3,6 +3,8 @@ package com.yumpro.ddogo.notice.service;
 import com.yumpro.ddogo.common.entity.Notice;
 import com.yumpro.ddogo.common.entity.User;
 import com.yumpro.ddogo.notice.reprository.NoticeRepository;
+import com.yumpro.ddogo.notice.validation.Noticeform;
+import com.yumpro.ddogo.user.exception.DataNotFoundException;
 import com.yumpro.ddogo.user.reprository.UserRepository;
 import com.yumpro.ddogo.user.validation.UserCreateForm;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +17,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -41,5 +44,35 @@ public class NoticeService {
         noticeRepository.save(notice);
     }
 
+    // notiNo값을 주면 notice 값을
+    public Notice getNotice(Integer notiNo){
+        System.out.println("getNotice진입");
+        Optional<Notice> notice = noticeRepository.findById(notiNo);
+        if(notice.isPresent()){
+            return notice.get();
+        }else {
+            throw new DataNotFoundException("notice not Found");
+        }
+    }
 
+    // 공지사항 수정 폼 Notice -> Noticeform
+    public Noticeform toNoticeform(Notice notice) {
+        Noticeform noticeform = new Noticeform();
+        noticeform.setNotiTitle(notice.getNotiTitle());
+        noticeform.setNotiContent(notice.getNotiContent());
+       return noticeform;
+    }
+
+
+    //수정하기
+    public void modify(Notice notice, Noticeform noticeform) {
+        notice.setNotiContent(noticeform.getNotiContent());
+        notice.setNotiTitle(noticeform.getNotiTitle());
+        noticeRepository.save(notice);
+    }
+
+    //공지사항 삭제하
+    public void noticeDelete(Notice notice) {
+        noticeRepository.delete(notice);
+    }
 }
